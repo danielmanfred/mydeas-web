dd3787f8ece03f526b8a004f6937f8b55efb43c1
danielmanfred <danielmanfred@gmail.com>
2018-05-26 21:09:39 -0300

    Adicionado componente usuário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Mydeas';
+    arthur = {
+        name: 'Arthur Dent',
+        email: 'arthurdent@gmail.com',
+        gender: 'M',
+        isAdmin: false
+    }
+
+    ford = {
+        name: 'Ford Prefect',
+        email: 'fordprefect@gmail.com',
+        isAdmin: true
+    }
+
+    trillian = {
+        name: 'Trillian',
+        email: 'trillian@gmail.com',
+        gender: 'F',
+        isAdmin: false
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
